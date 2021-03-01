f92fd140fc504b7f7a0a158834430aa810317a1c
Ravichandra <ravichandrachakka8@gmail.com>
2021-03-01 22:46:23 +0530

    Network Connectivity using Live Data.

--- app/src/main/java/com/ravi/kotlin_mvvm_with_retrofit/ui/photos/MainActivity.kt
+++ app/src/main/java/com/ravi/kotlin_mvvm_with_retrofit/ui/photos/MainActivity.kt
@@ -3,8 +3,11 @@ package com.ravi.kotlin_mvvm_with_retrofit.ui.photos
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.observe
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.ravi.kotlin_mvvm_with_retrofit.databinding.ActivityMainBinding
@@ -13,50 +16,59 @@ import com.ravi.kotlin_mvvm_with_retrofit.ui.ViewModelproviderFactory
 import com.ravi.kotlin_mvvm_with_retrofit.ui.photos.adapter.PhotoAdapter
 import com.ravi.kotlin_mvvm_with_retrofit.ui.photos.model.PhotoResponse
 import com.ravi.kotlin_mvvm_with_retrofit.ui.photos.viewmodel.PhotoViewModel
+import com.ravi.kotlin_mvvm_with_retrofit.utils.ConnectionLiveData
 import com.ravi.kotlin_mvvm_with_retrofit.utils.Resource
 import kotlinx.coroutines.flow.collect
 
-class MainActivity : AppCompatActivity(),SelectedCallBack {
+class MainActivity : AppCompatActivity(), SelectedCallBack {
     private lateinit var photoViewModel: PhotoViewModel
     private lateinit var photoList: MutableList<PhotoResponse>
     private lateinit var recyclerViewAdapter: PhotoAdapter
-    private lateinit var activityMainBinding:ActivityMainBinding
+    private lateinit var activityMainBinding: ActivityMainBinding
+    private lateinit var connectionLiveData: ConnectionLiveData
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-         activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(activityMainBinding.root)
 
-        val mediaRepository:MediaRepository = MediaRepository()
+        connectionLiveData = ConnectionLiveData(this)
+        connectionLiveData.observe(this, Observer { flag ->
+            if(flag){
+                photoViewModel.getPhotoInformation()
+            }else{
+                Toast.makeText(this,"Please check internet and try again",Toast.LENGTH_LONG).show()
+            }
+        })
+
+        val mediaRepository: MediaRepository = MediaRepository()
         val viewModelFactory = ViewModelproviderFactory(PhotoViewModel(mediaRepository))
 
-         photoViewModel = ViewModelProviders.of(this, viewModelFactory).get(PhotoViewModel::class.java)
+        photoViewModel = ViewModelProviders.of(this, viewModelFactory).get(PhotoViewModel::class.java)
 
         val linearLayoutManager = LinearLayoutManager(this)
         activityMainBinding.photoRecyclerView.layoutManager = linearLayoutManager
 
         activityMainBinding.photoRecyclerView.addItemDecoration(
-            DividerItemDecoration(
-                this,
-                LinearLayoutManager.HORIZONTAL
-            )
+                DividerItemDecoration(
+                        this,
+                        LinearLayoutManager.HORIZONTAL
+                )
         )
 
         lifecycleScope.launchWhenStarted {
-            photoViewModel.getPhotoInformation()
+
 
             photoViewModel.photosMutableLiveData.collect {
-                when(it){
+                when (it) {
                     is Resource.Success -> {
-                            photoList = it.photoList;
-                             recyclerViewAdapter = PhotoAdapter(it.photoList,this@MainActivity)
-                            activityMainBinding.photoRecyclerView.adapter = recyclerViewAdapter
+                        photoList = it.photoList
+                        recyclerViewAdapter = PhotoAdapter(it.photoList, this@MainActivity)
+                        activityMainBinding.photoRecyclerView.adapter = recyclerViewAdapter
 
                     }
                 }
             }
         }
-
-
     }
 
     override fun onCheckboxSelected(pos: Int, checked: Boolean) {
